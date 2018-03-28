f3ed4c7bf8164fe6f4e38cbb56dca191ded5a842
changwu <changwu.me@gmail.com>
2018-03-28 15:45:33 +0800

    update config

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -1,5 +1,2 @@
-echo "Running filebeat service"
-service filebeat start
-
 echo "Launching bootstrap node"
 /usr/local/bin/pyethapp --unlock 3 --validate 3 --deposit 5000 -m 0 --password /root/.config/pyethapp/password.txt -l eth.chain:info,eth.chainservice:info,eth.validator:info --log-file /root/log/log.txt run
--- miner/start.sh
+++ miner/start.sh
@@ -1,5 +1,3 @@
-echo "Running filebeat service"
-service filebeat start
 echo "BOOTSTRAP_NODE is $BOOTSTRAP_NODE"
 if [ -z "$BOOTSTRAP_NODE" ]; then echo "BOOTSTRAP_NODE must be set" && exit 1; fi
 openssl rand -hex 32 > /root/.config/pyethapp/privkey.hex
